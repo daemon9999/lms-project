b9c334e7e28806026c4b8ff0540ca83243efbc64
daemon9999 <murad.mesimli.99@gmail.com>
2023-09-21 12:56:27 +0400

    shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      LMS Project
+      <p className="text-3xl font-medium text-sky-700">
+      Hello World
+    </p>
+
+    <Button variant={'destructive'}>Salam</Button>
     </div>
   )
 }
